6ef9069c2a6a9699f070850fd1daac61da6b95a2
Juni May <juni_may@outlook.com>
2022-09-27 10:01:44 +0800

    :new: Note: Password Cracking Using PCFGs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,23 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://JuniMay.github.io',
 	base: '/blog',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx({extendPlugins: false}), sitemap()],
+    markdown: {
+        remarkPlugins: [
+            remarkMath,
+        ],
+        rehypePlugins: [
+            [rehypeKatex, {
+            // Katex plugin options
+            }]
+        ],
+		extendDefaultPlugins: true
+    }
 });
